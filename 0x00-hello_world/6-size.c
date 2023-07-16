2945c931f96d6ea185c7597fc7dd2493bfbf1961
Bruce <brucengobeni2005@gmail.com>
2023-07-16 03:47:33 -0700

    Adding file 6-size.c for task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,13 +7,13 @@ int main(void)
 {
 int num;
 char letter;
-long int decimals;
+long int deci;
 long long int decimal;
 float last;
 
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(letter));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(num));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(decimals));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(deci));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(decimal));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(last));
 
