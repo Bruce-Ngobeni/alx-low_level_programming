29cd787abdf52a81dea423bed0d2beda47a60329
Bruce <brucengobeni2005@gmail.com>
2023-07-18 18:06:59 -0700

    Adding file 8-24_hours.c for task8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ *jack_bauer - a program that prints every minute of the day.
+ *Return: 0.
+ */
+void jack_bauer(void)
+{
+	int first = 0;
+	int second = 0;
+	int third = 0;
+	int forth = 0;
+
+	while (first < 3)
+	{
+		second = 0;
+		while (second < 4)
+		{
+			third = 0;
+			while (third < 6)
+			{
+				forth = 0;
+				while (forth < 10)
+				{
+					_putchar(48 + first);
+					_putchar(48 + second);
+					_putchar(':');
+					_putchar(48 + third);
+					_putchar(48 + forth);
+					_putchar('\n');
+					forth++;
+				}
+				third++;
+			}
+			second++;
+		}
+		first++;
+	}
+}
