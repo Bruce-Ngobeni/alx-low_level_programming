d3dc15d41347a6f06adad17a7a89ac33c0d1df87
Bruce <brucengobeni2005@gmail.com>
2023-07-18 19:14:17 -0700

    Adding file 10-add.c for task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *add - a program that adds two integers.
+ *Return: 0.
+ *@n: parameter for one integer
+ *@c: parameter for the other integer.
+ */
+int add(int n, int c)
+{
+	int result;
+
+	result = n + c;
+	return (result);
+}
