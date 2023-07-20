e9b8659a89ff4fe6742293cec63679429d6cdfc7
Bruce <brucengobeni2005@gmail.com>
2023-07-20 15:16:00 -0700

    Adding file 9-fizz_buzz.c for task9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main -  program that prints the numbers from 1 to 100.
+ *Return: 0 success.
+ */
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%i", i);
+		}
+		if (i == 100)
+		{
+			break;
+		}
+		printf(" ");
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
