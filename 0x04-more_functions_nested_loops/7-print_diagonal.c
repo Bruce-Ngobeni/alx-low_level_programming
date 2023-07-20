54afcfa145596b26914d2b60f4f4f5461f8dd1cc
Bruce <brucengobeni2005@gmail.com>
2023-07-20 13:09:58 -0700

    Adding file 7-print_diagonal.c for task7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *print_diagonal - function that draws a diagonal line.
+ *@n: input parameter from the user.
+ *Return: 0 Success.
+ */
+void print_diagonal(int n)
+{
+	int i = 0, j = 0;
+
+	while (i < n)
+	{
+		j = 0;
+		while (j <= i)
+		{
+			if (j == i)
+			{
+				_putchar(92);
+			}
+			 _putchar(32);
+			 j++;
+		}
+		_putchar(10);
+		i++;
+	}
+	if (n <= 0)
+	{
+		_putchar(10);
+	}
+}
