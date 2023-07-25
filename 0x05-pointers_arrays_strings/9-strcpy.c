c0180566f0296636f317c42bbfb188e7bcc55429
Bruce <brucengobeni2005@gmail.com>
2023-07-25 11:35:31 -0700

    Adding file 9-strcpy.c for task9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+/**
+ * _strcpy - function that copies a string.
+ * @dest: a pointer to copy the string to.
+ * @src: a pointer holding an address of where the string in memory.
+ * Return: the pointer to dest.
+ */
+#include "main.h"
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	char *n =  dest;
+
+	while (src[i] != '\0')
+	{
+		n[i] = src[i];
+		i++;
+	}
+	n[i + 1] = '\0';
+
+	return (n);
+}
