d05641efa1068ed4186f3d223540d9274b78a7bf
Bruce <brucengobeni2005@gmail.com>
2023-07-25 07:20:12 -0700

    Adding file 7-puts_half.c for task7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+/**
+ * puts_half - function that prints half of a string
+ * @str: a pointer argument which can hold an adress of an char type.
+ *
+ * Return: nothing
+ */
+#include "main.h"
+
+void puts_half(char *str)
+{
+	int i = 0, n, m;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if ((i % 2) != 0)
+	{
+		n = (i - 1) / 2;
+		m = i - n;
+		while (m <= i)
+		{
+			m++;
+			_putchar(str[m]);
+		}
+	}
+	else
+	{
+		n = i / 2;
+		while (n <= i)
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+	_putchar('\n');
+}
