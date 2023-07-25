2f9994170caab3c1ea53818ad06454cbf245b9bc
Bruce <brucengobeni2005@gmail.com>
2023-07-25 08:42:44 -0700

    Adding file 8-print_array.c for task8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,13 +15,10 @@ void print_array(int *a, int n)
 
 	while (i < n)
 	{
+		printf("%i, ", a[i]);
 		if (i == (n - 1))
 		{
 			printf("%i\n", a[i]);
-		}
-		else
-		{
-			printf("%i, ", a[i]);
 		}
 		i++;
 	}
