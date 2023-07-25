d643c6e3129f66c706be4daac550fa882f6f9093
Bruce <brucengobeni2005@gmail.com>
2023-07-24 17:41:21 -0700

    Adding file 4-print_rev.c for task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+/**
+ * print_rev - function that prints a tring in reverse.
+ * @s: a pointer argument which can only take adress of char type.
+ *
+ *Return: nothing.
+ */
+#include "main.h"
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int length = 0;
+
+	while (i < 1000)
+	{
+		if ((*(s + i)) == '\0')
+		{
+			break;
+		}
+		i++;
+		length = i;
+	}
+	while (length >= 0)
+	{
+		_putchar(*(s + length));
+		length--;
+	}
+	_putchar('\n');
+}
