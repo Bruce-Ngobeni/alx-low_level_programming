5714aec61fb6da462795279b2a42293b1276a652
Bruce <brucengobeni2005@gmail.com>
2023-08-01 15:56:58 -0700

    Adding file 100-set_string.c for task8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+/**
+ * set_string - a function that sets the value of a pointer to a char.
+ * @s: a pointer pointing a location of a string.
+ * @to: a poniter pointing a location of a string.
+ *
+ * Return: nothing.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
