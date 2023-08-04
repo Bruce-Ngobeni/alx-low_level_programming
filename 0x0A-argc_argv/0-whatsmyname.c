e3323534acdf736ddc5be8c412490baef7be8bf1
Bruce <brucengobeni2005@gmail.com>
2023-08-04 16:28:12 -0700

    Adding file 0-whatsmyname.c for task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+/**
+ * main - a function that prints its name.
+ * @argc: an argument that denotes the number of arguments given
+ * during program execution.
+ * @argv: is a pointer array pointing to each parameter passed to the program.
+ *
+ * Return: 0 always.
+ */
+#include <stdio.h>
+#include "main.h"
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n",argv[0]);
+	return (0);
+}
